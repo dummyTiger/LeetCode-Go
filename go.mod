506920e0eae2385e15dbe4cf6eb65148f5346858
guchao <guchao@mgtv.com>
2021-06-25 23:55:16 +0800

    first commit,add two sum

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dummyTiger/LeetCode-Go
+
+go 1.13
---
+++ solution/1.Two-Sum/Two Sum.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for k, v := range nums {
+		if index, ok := m[target-v]; ok {
+			return []int{index, k}
+		}
+		m[v] = k
+	}
+	return nil
+}
