9e2d48bef1cd65a38a80c5ee26001dc249697e8c
star <star@stardeMacBook-Pro.local>
2021-06-29 06:54:01 +0800

    format

--- solution/0026-RemoveDuplicatesfromSortedArray/RemoveDuplicates_test.go
+++ solution/0026-RemoveDuplicatesfromSortedArray/RemoveDuplicates_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestRemoveDuplicates(t *testing.T) {
 	nums1 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
-	nums2:=[]int{1,1,2}
+	nums2 := []int{1, 1, 2}
 	assert.Equal(t, 5, removeDuplicates(nums1))
 	assert.Equal(t, 2, removeDuplicates(nums2))
 
