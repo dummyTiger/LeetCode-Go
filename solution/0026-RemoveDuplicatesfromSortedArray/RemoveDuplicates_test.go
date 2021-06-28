b33419185bbb1973d2acdfbed95eb8d0338b4941
dummyTiger <g.c198664@gmail.com>
2021-06-29 06:58:19 +0800

    add test case

--- solution/0026-RemoveDuplicatesfromSortedArray/RemoveDuplicates_test.go
+++ solution/0026-RemoveDuplicatesfromSortedArray/RemoveDuplicates_test.go
@@ -8,7 +8,9 @@ import (
 func TestRemoveDuplicates(t *testing.T) {
 	nums1 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
 	nums2 := []int{1, 1, 2}
+	nums3 := []int{1, 1, 1}
 	assert.Equal(t, 5, removeDuplicates(nums1))
 	assert.Equal(t, 2, removeDuplicates(nums2))
+	assert.Equal(t, 2, removeDuplicates(nums3))
 
 }
